453fd61fbba5008692ef5f1759192066f82f4505
jonathanbutaye <jonathan.butaye@student.uhasselt.be>
2021-11-23 22:28:15 +0100

    constraint for instructioin

--- instruction.sv
+++ instruction.sv
@@ -6,7 +6,8 @@ class instruction;
 
     //constraint for ADD, ADC
     constraint instruction_constraint {
-        (instruction[7:4] inside {'h8});
+        //Fixate the 2 msb from the instruction to 10
+        (instruction[7:6] inside {'b10});
     }
     
     function new();
